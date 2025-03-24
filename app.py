efb3e6094a4d61862427a1c7a6b49ce139fd6063
Nishtha Hooda <nishthahooda@Nishthas-Macbook-air.local>
2025-03-24 23:54:09 +0530

    Initial commit for deployment

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+from flask_cors import CORS
+
+# Load the trained model
+model = joblib.load('fraud_detection_model.pkl')
+
+# Initialize Flask app
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+    # return "Credit Card Fraud Detection API is running!"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get JSON data from request
+        data = request.get_json()
+
+        # Convert to NumPy array and reshape
+        features = np.array(data['features']).reshape(1, -1)
+
+        # Predict
+        prediction = model.predict(features)[0]
+
+        # Return result
+        return jsonify({'fraud_prediction': int(prediction)})
+        # return render_template("index.html",prediction_text="The Prediction is ")
+
+    except Exception as e:
+        return jsonify({'error': str(e)}),400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
